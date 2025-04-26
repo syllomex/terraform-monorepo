6e02881838786d98ff6fdf52429409dc7f9cb925
Leonardo Santos <leonardojdossantos@gmail.com>
2025-04-25 22:14:00 -0300

    chore: build with webpack on local apply script

--- scripts/tf-apply.ts
+++ scripts/tf-apply.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
 import { spawnSync } from 'node:child_process';
 
+spawnSync('npx webpack', { stdio: 'inherit', shell: true });
 spawnSync('terraform apply', { stdio: 'inherit', shell: true });
